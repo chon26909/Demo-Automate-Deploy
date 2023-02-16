8a7f100e980c0a328d85e352df952b0dc140d058
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-02-16 23:59:20 +0700

    update app

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,7 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
+app.get("/", (req, res) => res.send("Hello World"));
+app.get("/env", (req, res) => res.json({ env: process.env }));
+
 app.listen(() => console.log("server listening on port " + port));
